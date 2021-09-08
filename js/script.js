ec95aea113b300861c0de5866748cb6124c8a7af
MiltonMalagon <miltonmalagon1985@gmail.com>
2021-09-08 16:01:02 -0500

    Added iteration and conditional statements

--- js/script.js
+++ js/script.js
@@ -18,11 +18,31 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 function showPage(list, page) {
-    const itemsPerPage = 9;
-    const startIndex = (page * itemsPerPage) - itemsPerPage;
-    const endIndex = page * itemsPerPage;
-    const studentList = document.querySelector(".student-list");
+    const itemsPerPage = 9
+    let startIndex = (page * itemsPerPage) - itemsPerPage;
+    let endIndex = page * itemsPerPage;
+    let studentList = document.querySelector(".student-list");
+
     studentList.innerHTML = "";
+
+    for (let i = 0; i < list.length; i++) {
+        if (i >= startIndex && i < endIndex) {
+            let studentItem = `
+               <li class="student-item cf">
+                  <div class="student-details">
+                     <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+                     <h3>${list[i].name.first} ${list[i].name.last}</h3>
+                     <span class="email">${list[i].email}</span>
+                  </div>
+                  <div class="joined-details">
+                     <span class="date">Joined ${list[i].registered.date}</span>
+                  </div>
+               </li>
+            `;
+
+            studentList.insertAdjacentHTML("beforeend", studentItem);
+        }
+    }
 }
 
 
@@ -34,3 +54,4 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+showPage(data, 1);
