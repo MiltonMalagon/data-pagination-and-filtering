92a700f57dd4a18f3fab603fee186ed827fe7c5d
MiltonMalagon <miltonmalagon1985@gmail.com>
2021-09-08 12:41:25 -0500

    Created function and index vars

--- js/script.js
+++ js/script.js
@@ -17,7 +17,11 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-
+function showPage(list, page) {
+    const itemsPerPage = 9;
+    const startIndex = (page * itemsPerPage) - itemsPerPage;
+    const endIndex = page * itemsPerPage;
+}
 
 
 /*
